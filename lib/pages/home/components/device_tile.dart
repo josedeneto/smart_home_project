30adb094c9869c8d38a543325b47ab7ad6ccd168
José De Neto <58984566+josedeneto@users.noreply.github.com>
2023-04-07 15:32:53 +0100

    Update device_tile.dart

--- lib/pages/home/components/device_tile.dart
+++ lib/pages/home/components/device_tile.dart
@@ -2,7 +2,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import 'pages/home/controller/home_page_controller.dart';
+import '../controller/home_page_controller.dart';
+
 
 class DeviceTile extends StatefulWidget {
   const DeviceTile(
